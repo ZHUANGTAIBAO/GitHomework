169b97737f49745fb7794681adee545d5e6d3032
ZHUANGTAIBAO <2296918265@qq.com>
2020-06-26 05:38:14 +0800

    修改过后的java程序-Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,31 @@
  public class Fibonacci {
 	
-	 public static int Fibonacci(int num) {
+	 public static int Fibonacci(int num)
+	{
 		
-		if(num == 1 || num == 2) {
-			return 1;
-		}else {
+		if(num == 1 || num == 2)
+			{
+				return 1;
+			}
+		else 
+			{
 		
-			return Fibonacci(num - 2) + Fibonacci(num - 1);
-		}
+				return Fibonacci(num - 2) + Fibonacci(num - 1);
+			}
 	}
 	
 	
-	 public static void main(String[] args) {
+	 public static void main(String[] args) 
+	{
 		
 	
-		for(int i = 1;i <= 200;i++) {
-
+		for(int i = 1;;i++)
+		 {
+			if (Fibonacci(i) >200)
+				break;
 			System.out.print(Fibonacci(i) + "\t");
+			
+			
 		}	
 	}
 	
